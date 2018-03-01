a09b9bda4104d69b004c9ae393ccdc35386763bf
Adrian Sims <adriansims62781@gmail.com>
2018-03-01 07:03:27 +0000

    Add project files.

---
+++ ProcLog/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcLog
+{
+    using System.Text.RegularExpressions;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 1)
+                {
+                    throw new Exception("You must supply a tcc log filename");
+                }
+                var filename = args[0];
+                var dateBracketPattern = @"\[(\d{2}/\d{2}/\d{2}) (\d{2}:\d{2}:\d{2})\]";
+                var logDict = new SortedDictionary<DateTime, SortedSet<int>>();
+                foreach (var line in File.ReadLines(filename))
+                {
+                    var regexResult = Regex.Match(line, dateBracketPattern);
+                    var dateGroup = regexResult.Groups[1];
+                    var timeGroup = regexResult.Groups[2];
+                    var time = timeGroup[0] - '0';
+                }
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add ProcLog/Program.cs
+git add ProcLog/App.config
+git add ProcLog/Properties/AssemblyInfo.cs
+git add ProcLog/ProcLog.csproj
+git add ProcLog.sln
+git add .gitignore
